953aad0e31ec78f720882306a8c00d606df31876
Tim Cools <tim@soloco.be>
2025-01-25 13:45:59 +0100

    Include lexy-language as git submodule

--- tests/specifications/runSingleSpecification.test.ts
+++ tests/specifications/runSingleSpecification.test.ts
@@ -3,7 +3,7 @@ import {createRunner} from "./createRunner";
 describe('runSingleSpecification', () => {
   it('specificFile',  async() => {
     const runner = createRunner();
-    runner.run(`../laws/Specifications/Function/ComplexVariables.lexy`);
+    runner.run(`tests/lexy-language/src/Specifications/Function/ComplexVariables.lexy`);
     //runner.run(`../laws/Specifications/ExecutionLogging/CallFunction.lexy`);
     //runner.run(`../laws/Specifications/Include/Include.lexy`);
     //runner.run(`../laws/Specifications/Scenario/UnknownParameterType.lexy`);
