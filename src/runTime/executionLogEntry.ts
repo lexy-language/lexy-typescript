c354cbdebb92fc932ccd9462b09ecde5ae9263c2
Tim Cools <tim@soloco.be>
2025-12-22 16:25:44 +0100

    Fix decmial issue

--- src/runTime/executionLogEntry.ts
+++ src/runTime/executionLogEntry.ts
@@ -43,9 +43,13 @@ export class ExecutionLogEntry {
   public deepCopy<T>(obj: T): T {
     if (obj === null) return null as T;
     if (obj instanceof Date) return new Date(obj) as T;
-    if (toString.call(obj) === '[object Decimal]' || obj === null) return obj.toNumber() as T;
-    if (typeof obj !== 'object' || obj === null) return obj;
+    if (toString.call(obj) === '[object Decimal]') return obj.toNumber() as T;
+    if (typeof obj !== 'object') return obj;
     if (Array.isArray(obj)) return obj.map(item => this.deepCopy(item)) as unknown as T;
+    return this.copyObject(obj);
+  }
+
+  private copyObject<T>(obj: T) {
     const copy = {} as { [K in keyof T]: T[K] };
     Object.keys(obj).forEach(key => {
       if (key.startsWith("__")) return;
