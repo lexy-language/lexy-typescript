c6a1f11131f40591166790827db9179476fa20a0
Tim Cools <tim@soloco.be>
2026-02-12 19:08:13 +0100

    Add filter criteria

--- src/parser/symbols/SuggestionsResult.ts
+++ src/parser/symbols/SuggestionsResult.ts
@@ -6,7 +6,7 @@ export class SuggestionsResult {
   public all: readonly Suggestion[] = [];
   public filter: string | null;
 
-  constructor(filtered: readonly Suggestion[] | null = null, all: readonly Suggestion[] | null = null, filter: string | null) {
+  constructor(filtered: readonly Suggestion[] | null = null, all: readonly Suggestion[] | null = null, filter: string | null = null) {
     this.filtered = filtered ? filtered : [];
     this.all = all ? all : [];
     this.filter = filter;
